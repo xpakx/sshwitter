a5c63cfd1cfd6d39aa9c54fc4f92a305124ac620
xpakx <55586470+xpakx@users.noreply.github.com>
2024-09-10 14:18:45 +0200

    feat: make a simple ssh server with password authentication

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/charmbracelet/log"
+	"github.com/charmbracelet/ssh"
+	"github.com/charmbracelet/wish"
+	"github.com/charmbracelet/wish/logging"
+)
+
+
+// TODO: move to yaml/env
+const (
+	host          = "localhost"
+	port          = "23230"
+	validPassword = "password"
+)
+
+func main() {
+	s, err := wish.NewServer(
+		wish.WithAddress(net.JoinHostPort(host, port)),
+		wish.WithHostKeyPath(".ssh/id_ed25519"),
+
+		// TODO: apply only for dev
+		wish.WithPasswordAuth(func(_ ssh.Context, password string) bool {
+			 if (password == validPassword) {
+				 log.Info("Successful authentication")
+			 } else {
+				 log.Info("Authentication failed")
+			 }
+			 return password == validPassword
+		 }),
+
+		wish.WithMiddleware(
+			logging.Middleware(),
+			func(next ssh.Handler) ssh.Handler {
+				return func(sess ssh.Session) {
+					wish.Println(sess, "Authorized!")
+				}
+			},
+		),
+	)
+
+
+	if err != nil {
+		log.Error("Could not start server", "error", err)
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	log.Info("Starting SSH server", "host", host, "port", port)
+	go func() {
+		if err = s.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+			log.Error("Could not start server", "error", err)
+			done <- nil
+		}
+	}()
+
+	<-done
+	log.Info("Stopping SSH server")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer func() { cancel() }()
+	if err := s.Shutdown(ctx); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+		log.Error("Could not stop server", "error", err)
+	}
+}
