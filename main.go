0f123c189a97467955acfaca1dfca6bd2c5ccde8
xpakx <55586470+xpakx@users.noreply.github.com>
2024-09-12 12:30:05 +0200

    feat: pass info about authenticated user to bubbletea program

--- main.go
+++ main.go
@@ -6,9 +6,9 @@ import (
 	"net"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
-	"fmt"
 
 	"github.com/charmbracelet/log"
 	"github.com/charmbracelet/ssh"
@@ -22,7 +22,6 @@ import (
 	"github.com/charmbracelet/wish/bubbletea"
 )
 
-
 // TODO: move to yaml/env
 const (
 	host          = "localhost"
@@ -31,8 +30,17 @@ const (
 	dev           = true
 )
 
+
+type SavedUser struct {
+	key        string
+	verified   bool
+}
 // TODO: move to db
-var users = map[string]string{
+var users = map[string]SavedUser{
+	"test" : { 
+		key: "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDQ2EOmuzeJo6LN4jg7Vuvf0BSjrqDMSguc3Z6i0zkVURz5Bb61BZlG7PpyqOQ1aeISNfoMSpVwx1fQytIvQBfpF6OX+XMI/wzgOEvbPNeQ0RHsPJjq0x6wMLNEWpPl5f07pDgBlWB8IkBTKvSZQje/WsEwDvUnFRrWcC8PHs2H/WRpm+wagg9T5N6jDqlC711DJEWIyKwl744QHK4NBnyXHfK+0pW/JfhEelyQ+bTVfWNDu9V5uZI69hiKZNs4UANhAoUEhhZIy60ZHho6Zn8JkZkjORMwGi/hi8lUaIDYXXcqKGqKQdU2HU5NgpWVO3/w7KRQceegDiMO5Aa/yMEtdVi0B2NmUGVTZcCEwkqWbACqG5r23AmgrMX/Hh8L/9Z1nFwnxCY2bUd29DQI1q7GzTwYIxNi9y7/8H5+gmU6Yn3Wm5mUjpxWLF9QbU0fOFNZ/WO1h3rRYCwoouJ4ixWuCM6BLcBuuEutx24mjBaO3x0p68XJ8rxMuvS/n9TwTywPfeDS5Yft1hHovyRt1vSAHLxd8eSP65vJHJwsYAL8psGbm68CyYnzf8D4CPSJh4DSCQRzNnfFjYozX9QuXAhPtJkjPI7w6mJyPmjUaDB+sOkolIqIdF0jBXuaB/Hv/03H3ul5+SqpB0s37Wh0rwI2ORX0Ct45pYjj78WtAkukEQ==",
+		verified: true,
+	},
 }
 
 func main() {
@@ -75,42 +83,70 @@ func main() {
 }
 
 
-func GetPasswordAuth(_ ssh.Context, password string) bool {
+func GetPasswordAuth(context ssh.Context, password string) bool {
 	if (password == validPassword) {
 		log.Info("Successful authentication")
 	} else {
 		log.Info("Authentication failed")
 	}
+	context.SetValue("guest", true);
+	context.SetValue("verified", false);
 	return password == validPassword
 }
 
-func GetPublicKeyAuth(_ ssh.Context, key ssh.PublicKey) bool {
-	log.Info("public-key")
-	for _, pubkey := range users {
+func GetPublicKeyAuth(context ssh.Context, key ssh.PublicKey) bool {
+	username := strings.Split(context.User(), ":")[0];
+	log.Infof("New connection with username: %s", username)
+	log.Info("Trying public key")
+	if savedUser, found :=  users[username]; found {
 		parsed, _, _, _, _ := ssh.ParseAuthorizedKey(
-			[]byte(pubkey),
+			[]byte(savedUser.key),
 		)
+		
 		if ssh.KeysEqual(key, parsed) {
+			context.SetValue("guest", false);
+			context.SetValue("verified", savedUser.verified);
 			return true
 		}
-	}
+	} 
+	log.Info("Public key not found")
+	context.SetValue("guest", true);
+	context.SetValue("verified", false);
 	return false
 }
 
 
 func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
+	username := strings.Split(s.Context().User(), ":")[0];
+	guest := s.Context().Value("guest").(bool)
+	verified := s.Context().Value("verified").(bool)
+	log.Info("New tea handler", "user", username, "guest", guest, "verified", verified)
+
 	renderer := bubbletea.MakeRenderer(s)
 	txtStyle := renderer.NewStyle().Foreground(lipgloss.Color("10"))
 	quitStyle := renderer.NewStyle().Foreground(lipgloss.Color("8"))
+	usernameStyle := renderer.NewStyle().Foreground(lipgloss.Color("5"))
 
-	m := model{ name: "sshwitter", txtStyle: txtStyle, quitStyle: quitStyle }
+	m := model{ 
+		name: "sshwitter", 
+		username: username,
+		txtStyle: txtStyle, 
+		quitStyle: quitStyle,
+		userStyle: usernameStyle,
+		guest: guest,
+		verified: verified,
+	}
 	return m, []tea.ProgramOption{tea.WithAltScreen()}
 }
 
 type model struct {
-	name      string
-	txtStyle  lipgloss.Style
+	name       string
+	username   string
+	txtStyle   lipgloss.Style
 	quitStyle  lipgloss.Style
+	userStyle  lipgloss.Style
+	guest      bool
+	verified   bool
 }
 
 func (m model) Init() tea.Cmd {
@@ -129,6 +165,10 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	s := fmt.Sprintf("Authorized!")
-	return m.txtStyle.Render(s) + "\n\n" + m.quitStyle.Render("Press 'q' to quit\n")
+	return m.txtStyle.Render("Authorized!") + 
+		"\n" + 
+		"Hello, " + 
+		m.userStyle.Render(m.username) +
+		"\n\n" + 
+		m.quitStyle.Render("Press 'q' to quit\n")
 }
