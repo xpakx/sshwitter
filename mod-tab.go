ed4aa847e8224177474aa1de61fb8a6a6b42511f
xpakx <55586470+xpakx@users.noreply.github.com>
2024-09-25 12:34:23 +0200

    feat: use tabular view for unverified users

--- mod-tab.go
+++ mod-tab.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"database/sql"
+	"strconv"
 	"strings"
 
+	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/log"
@@ -16,6 +18,28 @@ func getModeratorTab(renderer *lipgloss.Renderer, db *sql.DB) (ModeratorTabModel
 	if (err != nil) {
 		log.Debug("Error while fetching users")
 	}
+
+
+	columns := []table.Column{
+		{Title: "Id", Width: 3},
+		{Title: "Username", Width: 10},
+		{Title: "Email", Width: 10},
+		{Title: "Birth date", Width: 10},
+	}
+
+	var rows []table.Row = make([]table.Row, 0, len(unverifiedUsers))
+	for _, user  := range unverifiedUsers {
+		rows = append(rows, table.Row{strconv.Itoa(int(user.id)), user.username, user.email, ""})
+		
+	}
+	table := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+		table.WithFocused(true),
+		table.WithHeight(10),
+	)
+
+
 	prefixStyle := renderer.NewStyle().
 			Foreground(lipgloss.Color("#1da1f2"))
 
@@ -27,8 +51,10 @@ func getModeratorTab(renderer *lipgloss.Renderer, db *sql.DB) (ModeratorTabModel
 		users: unverifiedUsers,
 		current: 0,
 		db: db,
+		table: table,
 	}
 }
+
 
 type ModeratorTabModel struct {
 	txtStyle     lipgloss.Style
@@ -38,45 +64,72 @@ type ModeratorTabModel struct {
 	users        []SavedUser
 	current      int
 	db           *sql.DB
+	table        table.Model
 }
 
 func (m ModeratorTabModel) Init() tea.Cmd {
 	return nil
 }
 
+func (m ModeratorTabModel) GetCurrentIndex() int {
+	username := m.table.SelectedRow()[1]
+	for i, user := range m.users {
+		if username == user.username {
+			return i
+		}
+	}
+	return -1
+}
+
 
 func (m *ModeratorTabModel) RemoveCurrentFromList() {
-	m.users = append(m.users[:m.current], m.users[m.current+1:]...)
-	m.current = max(m.current - 1, 0);
+	current := m.GetCurrentIndex()
+	if current < 0 {
+		return
+	}
+	m.users = append(m.users[:current], m.users[current+1:]...)
+	var rows []table.Row = make([]table.Row, 0, len(m.users))
+	for _, user  := range m.users {
+		rows = append(rows, table.Row{strconv.Itoa(int(user.id)), user.username, user.email, ""})
+	}
+	m.table.SetRows(rows)
 }
 
 func (m ModeratorTabModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "q", "ctrl+c":
 			return m, tea.Quit
-		case "j", "down": 
-			m.current = min(m.current + 1, len(m.users)-1);
-			return m, nil
-		case "k", "up": 
-			m.current = max(m.current - 1, 0);
-			return m, nil
 		case "enter":
 			if len(m.users) > 0 {
 				// TODO: move to command
-				AcceptUser(m.db, m.users[m.current])
+				user := m.GetCurrentChoice()
+				AcceptUser(m.db, user)
 				m.RemoveCurrentFromList()
 			}
 		case "delete":
 			if len(m.users) > 0 {
 				// TODO: move to command
-				DeleteUser(m.db, m.users[m.current])
+				user := m.GetCurrentChoice()
+				DeleteUser(m.db, user)
 				m.RemoveCurrentFromList()
 			}
 		}
 	}
-	return m, nil
+	m.table, cmd = m.table.Update(msg)
+	return m, cmd
+}
+
+func (m ModeratorTabModel) GetCurrentChoice() SavedUser {
+	username := m.table.SelectedRow()[1]
+	for _, user := range m.users {
+		if username == user.username {
+			return user
+		}
+	}
+	return SavedUser{}
 }
 
 func (m ModeratorTabModel) View() string {
@@ -88,15 +141,11 @@ func (m ModeratorTabModel) View() string {
 	doc.WriteString("\n\n")
 
 	if len(m.users) > 0 {
-		for i, user := range m.users {
-			doc.WriteString(m.getPrefix(i))
-			doc.WriteString(user.username)
-			doc.WriteString("\n")
-
-		}
+		doc.WriteString(m.table.View())
 	} else {
 		doc.WriteString(m.quitStyle.Render("No users"))
 	}
+
 
 	return doc.String()
 }
