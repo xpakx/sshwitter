43216255988d37725693ddd0b9fba2bcbad05bc1
xpakx <55586470+xpakx@users.noreply.github.com>
2024-11-05 17:04:19 +0100

    feat: add method for getting replies for post

--- post-repo.go
+++ post-repo.go
@@ -235,3 +235,35 @@ func GetPostById(db *sql.DB, id int64, username string) (Post, bool) {
 
 	return post, true
 }
+
+func FindReplies(db *sql.DB, id int64, viewer SavedUser) ([]Post, error) {
+	query := `
+	SELECT p.id, p.content, p.user_id, p.created_at, u.username, p.likes,
+	       CASE WHEN l.user_id IS NOT NULL THEN TRUE ELSE FALSE END AS liked
+	FROM posts p
+	LEFT JOIN likes l ON p.id = l.post_id AND l.user_id = $1
+	LEFT JOIN users u
+	ON p.user_id = u.id
+	WHERe p.parent_id = $2
+	ORDER BY p.created_at DESC`
+	rows, err := db.Query(query, viewer.id, id)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var posts []Post
+	for rows.Next() {
+		var post Post
+		if err := rows.Scan(&post.id, &post.content, &post.userId, &post.createdAt, &post.username, &post.likes, &post.liked); err != nil {
+			return nil, err
+		}
+		posts = append(posts, post)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return posts, nil
+}
